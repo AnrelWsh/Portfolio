0048814d42df456422d88b547313f12a7b3b40ef
Angel Hmeli <angel.hmeli@edu.devinci.fr>
2022-10-18 16:28:20 +0200

    vrai commencement v2

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-//import MagnetMouse from 'magnet-mouse'
-
-window.onscroll = function() {scrollFunction()};
+/*window.onscroll = function() {scrollFunction()};
 const logoGif = document.getElementById("logo")
 function scrollFunction() {
   if (document.documentElement.scrollTop >= 100) {
-    logoGif.style.position = 'fixed';
     logoGif.style.width = "10%";
-    logoGif.style.gridArea = "logoAfter";
-    logoGif.style.margin ='2%';
     
   } else {
-    logoGif.style.position = 'static';
     logoGif.style.width = "70%";
-    logoGif.style.gridArea = "logoInit";
-    logoGif.style.margin = 'auto';
     
   }
-}
+}*/
+
 let mm = new MagnetMouse({
   magnet: {
     element: '.magnet'
